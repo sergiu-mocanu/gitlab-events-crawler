b020c5c527b20ebafd011ad04cf1c4369603fee3
Sergiu Mocanu <sergiu.mocanu.dev@outlook.com>
2025-10-03 12:41:21 +0200

    Project restructure: split into sub-modules

---
+++ src/gitlab_crawler/models.py
@@ -0,0 +1,251 @@
+import orjson
+import pandas as pd
+
+from typing import Optional, Dict, TypedDict, cast
+from datetime import datetime
+
+from gitlab_crawler.utils import datetime_to_str, total_and_avg_time
+from gitlab_crawler.types_formats import Timestamp
+
+
+class GitLabToken:
+    """
+        GitLab token, the value is the one you get when you create the token on the forge website and is read from the file file_name.
+        Attributes:
+          file_name (str): file path that contains the token.
+          value (str): the token to pass in the request.
+    """
+
+    def __init__(self, file_name: str):
+        self.file_name: str = file_name
+        with open(self.file_name, "r") as token_file:
+            token = token_file.read().strip()
+            self.value: str = token
+
+
+    def print_object(self):
+        print(f"ForgeToken: file_name = {self.file_name} - value = {self.value}")
+
+
+class GitLabInstance:
+    """
+        The GitLab instance for recovering the most recent updated projects' metadata. You shall have created a token before.
+
+        Attributes:
+          gl_instance (str): name of the GitLab instance (e.g., gitlab.com, gitlab.softwareheritage.org)
+          url (str): endpoint URL for obtaining GitLab projects by recent activity
+    """
+
+    def __init__(self, gl_instance: str):
+        self.raw_name = gl_instance
+
+        if 'https://' not in gl_instance:
+            self.gl_instance = 'https://' + gl_instance + '/'
+        else:
+            self.gl_instance = gl_instance
+
+        self.url = self.gl_instance + 'api/v4/projects?order_by=last_activity_at'
+
+
+    def __getattr__(self, name):
+        if name == 'name':
+            value = self.gl_instance
+        elif name == 'raw_name':
+            value = self.raw_name
+        elif name == 'url':
+            value = self.url
+        else:
+            raise AttributeError(name)
+        return value
+
+
+    def print_object(self):
+        print(f'Instance = {self.gl_instance} - url = {self.url}')
+
+
+"""
+Dictionary templates used for the activity stats written to JSON/CSV files
+"""
+class CrawlerConfigDict(TypedDict):
+    instance: str
+    trigger_frequency: int
+
+
+class BacklogProjects(TypedDict):
+    backlog_time_window: str
+    nb_recovered_projects: int
+    recovery_time: float
+    response_time_average: float
+    response_time_list: list[float]
+
+
+class BacklogEvents(TypedDict):
+    events_recovery_time: str
+    nb_recovered_events: int
+    avg_response_time: float
+    response_time: list[float]
+
+
+class DiskWriteTimestamp(TypedDict):
+    nb_events: int
+    write_duration: float
+
+
+class DiskWriteHourly(TypedDict):
+    total_write_duration: float
+    hourly_event_writes: Dict[Timestamp, DiskWriteTimestamp]
+
+
+class TriggerProjectRecovery(TypedDict):
+    nb_recovered_projects: int
+    projects_recovery_time: float
+    projects_response_time_avg: float
+    projects_response_time_list: list[float]
+
+
+class TriggerEventRecovery(TypedDict):
+    nb_processed_projects: int
+    nb_recovered_events: int
+    events_response_time_avg: float
+    events_response_time_total: str
+    events_response_time_list: list[float]
+
+
+class TriggerStatsDict(TypedDict):
+    project_recovery: TriggerProjectRecovery
+    event_recovery: TriggerEventRecovery
+    total_processing_time: float
+    nb_request_errors: int
+
+
+# Top-level dict
+class OverallStats(TypedDict):
+    crawler_config: CrawlerConfigDict
+    backlog_projects_recovery: BacklogProjects
+    backlog_events_recovery: BacklogEvents
+    event_disk_writes: Dict[Timestamp, DiskWriteHourly]
+    crawls: Dict[Timestamp, TriggerStatsDict]
+
+
+class CSVStats(TypedDict):
+    datetime: str
+    weekday: str
+    nb_recovered_projects: int
+    projects_avg_response_time: float
+    nb_recovered_events: int
+    events_avg_response_time: float
+    processing_time: float
+    nb_request_errors: int
+
+
+class ActivityStats:
+
+    def __init__(self, gl_instance: str, trigger_frequency: int):
+        self.data: OverallStats = {
+            'crawler_config': {'instance': gl_instance, 'trigger_frequency': trigger_frequency},
+            'backlog_projects_recovery': cast(BacklogProjects, {}),
+            'backlog_events_recovery': cast(BacklogEvents, {}),
+            'event_disk_writes': {},
+            'crawls': {}
+        }
+        self.trigger_projects_recovery = cast(TriggerProjectRecovery, {})
+        self.trigger_events_recovery = cast(TriggerEventRecovery, {})
+        self.csv_stats: list[CSVStats] = []
+        self.hourly_event_writes: Dict[Timestamp, DiskWriteTimestamp] = {}
+        self.json_file_path: Optional[str] = None
+        self.csv_file_path: Optional[str] = None
+
+
+    def set_file_path(self, *, json_path: str, csv_path: str):
+        self.json_file_path = json_path
+        self.csv_file_path = csv_path
+
+
+    def set_config(self, instance: str, trigger_frequency: int):
+        self.data['crawler_config'] = CrawlerConfigDict(instance=instance, trigger_frequency=trigger_frequency)
+
+
+    def set_backlog_projects(self, *, time_window: str, nb_projects: int,
+                             recovery_time: float, list_response_time: list[float]):
+        _, avg_response_time = total_and_avg_time(list_response_time)
+
+        self.data['backlog_projects_recovery'] = BacklogProjects(backlog_time_window=time_window,
+                                                                 nb_recovered_projects=nb_projects,
+                                                                 recovery_time=recovery_time,
+                                                                 response_time_average=avg_response_time,
+                                                                 response_time_list=list_response_time)
+
+
+    def set_backlog_events(self, *, time_elapsed: str, nb_events: int, response_time: list[float]):
+        _, avg_response_time = total_and_avg_time(response_time)
+        self.data['backlog_events_recovery'] = BacklogEvents(events_recovery_time=time_elapsed,
+                                                             nb_recovered_events=nb_events,
+                                                             avg_response_time=avg_response_time,
+                                                             response_time=response_time)
+
+
+    def add_disk_write_entry(self, timestamp: Timestamp, nb_events: int, write_duration: float):
+        disk_write_entry: DiskWriteTimestamp = {'nb_events': nb_events, 'write_duration': write_duration}
+        self.hourly_event_writes.update({timestamp: disk_write_entry})
+
+
+    def set_overall_disk_write(self, current_timestamp: Timestamp, write_duration: float):
+        hourly_write_entry = DiskWriteHourly(total_write_duration=write_duration,
+                                             hourly_event_writes=self.hourly_event_writes)
+        self.data['event_disk_writes'].update({current_timestamp: hourly_write_entry})
+        self.hourly_event_writes.clear()
+
+
+    def set_trigger_projects(self, nb_projects: int, recovery_time: float, list_response_time: list[float]):
+        _, avg_response_time = total_and_avg_time(list_response_time)
+        self.trigger_projects_recovery = {'nb_recovered_projects': nb_projects,
+                                          'projects_recovery_time': recovery_time,
+                                          'projects_response_time_avg': avg_response_time,
+                                          'projects_response_time_list': list_response_time}
+
+
+    def set_trigger_events(self, *, nb_events: int, list_response_time: list[float]):
+        total_response_time, avg_response_time = total_and_avg_time(list_response_time)
+        self.trigger_events_recovery = {'nb_recovered_events': nb_events,
+                                        'events_response_time_avg': avg_response_time,
+                                        'events_response_time_total': total_response_time,
+                                        'events_response_time_list': list_response_time}
+
+
+    def set_trigger_stats(self, *, current_time: datetime, processing_time: float, nb_errors: int):
+        stats_entry: TriggerStatsDict = {'project_recovery': self.trigger_projects_recovery,
+                                         'event_recovery': self.trigger_events_recovery,
+                                         'total_processing_time': processing_time,
+                                         'nb_request_errors': nb_errors}
+
+        timestamp = datetime_to_str(current_time)
+        self.data['crawls'][timestamp] = stats_entry
+
+
+    def append_csv_stats(self, *, current_time: datetime, nb_projects: int, projects_avg_time: float,
+                      nb_events: int, events_avg_response_time: float, processing_time: float, nb_errors: int):
+
+        # Extract the day of the week (e.g., Mon, Tue, Wen)
+        weekday = current_time.strftime('%a')
+        timestamp = datetime_to_str(current_time)
+
+        csv_entry: CSVStats = {'datetime': timestamp,
+                          'weekday': weekday,
+                          'nb_recovered_projects': nb_projects,
+                          'projects_avg_response_time': projects_avg_time,
+                          'nb_recovered_events': nb_events,
+                          'events_avg_response_time': events_avg_response_time,
+                          'processing_time': processing_time,
+                          'nb_request_errors': nb_errors}
+
+        self.csv_stats.append(csv_entry)
+
+
+    def disk_write_json(self):
+        with open(self.json_file_path, 'wb') as stats_file:
+            stats_file.write(orjson.dumps(self.data))
+
+
+    def disk_write_csv(self):
+        stats_df_file = pd.DataFrame.from_records(self.csv_stats)
+        stats_df_file.to_csv(self.csv_file_path, index=False)
---
+++ src/gitlab_crawler/trackers.py
@@ -0,0 +1,279 @@
+import time
+
+from typing import Dict, Optional, TypedDict
+
+from bisect import insort
+
+from gitlab_crawler.types_formats import *
+
+
+class CrawlProcessingTimer:
+    """
+    Timer used to measure the processing time of recovering all the projects and events.
+    Timer launched after the crawler initialization (i.e., after all the backlog projects and events are recovered)
+    """
+
+    def __init__(self):
+        self._timer_start: Optional[time] = None
+        self._elapsed_time: Optional[time] = None
+        self._first_processing_measured = False
+
+
+    def start_timer(self):
+        self._timer_start = time.time()
+
+
+    def stop_timer(self):
+        timer_end = time.time()
+        self._elapsed_time = timer_end - self._timer_start
+
+        if not self._first_processing_measured:
+            self._first_processing_measured = True
+
+        self._timer_start = None
+
+
+    def get_elapsed_time(self):
+        return self._elapsed_time
+
+
+    def is_timer_started(self):
+        return self._timer_start is not None
+
+
+    def is_first_processing_measured(self):
+        return self._first_processing_measured
+
+
+class BacklogTracker:
+    """
+    Track the number of 'backlog' projects and events recovered during the crawler initialization
+
+    'Backlog' projects:
+    Due to the behavior of the GitLab API, the 'last_activity_at' field of a project (which is used to filter the
+    projects with recent activity) is updated at most once per 1h. Therefore, in order to guarantee the recovery of all
+    the events created during the crawler start hour, it is necessary to recover all the projects that have been updated
+    since the beginning of the previous hour (i.e., if the crawler is launched at 13h30, it will look for projects
+    updated since 12h00): called backlog projects.
+
+    'Backlog' events:
+    In the context of the example from above, the 'backlog' events are events that were created since the start of the
+    hour of the crawler launch (from 13h00 to 13h30). However, due to the potentially considerable amount of projects
+    updated since 12h00, the 'backlog' event recovery can take up to 20 minutes.
+    """
+
+    def __init__(self):
+        self._nb_backlog_projects: int = 0
+        self._nb_backlog_projects_total: int = 0
+        self._backlog_events_recovered: bool = False
+        self._nb_backlog_events: int = 0
+
+
+    def set_nb_projects(self, nb_projects: int):
+        self._nb_backlog_projects = nb_projects
+        self._nb_backlog_projects_total = nb_projects
+
+
+    def decr_nb_projects(self):
+        self._nb_backlog_projects -= 1
+
+
+    def get_nb_backlog_projects(self):
+        return self._nb_backlog_projects
+
+
+    def are_all_projects_processed(self):
+        return self._nb_backlog_projects == 0
+
+
+    def incr_nb_events(self, nb_events: int):
+        self._nb_backlog_events += nb_events
+
+
+    def get_nb_events(self):
+        return self._nb_backlog_events
+
+
+    def events_recovered(self):
+        return self._backlog_events_recovered
+
+
+    def set_recovery_finished(self):
+        self._backlog_events_recovered = True
+
+
+    def is_recovery_finished(self):
+        return self._backlog_events_recovered
+
+
+class HourlyEvents(TypedDict):
+    fetched_new_events: bool
+    events: list[GitLabEvent]
+
+
+class ProjectsEventsTracker:
+    """
+    Store and manage the recovered projects and events (sorted by hour)
+
+    Attributes:
+          _recent_projects_ids: recovered projects that haven't been treated (no events recovered)
+          _known_events: list of events id (and the corresponding project id) that have been stored
+          _timestamp_events: dictionary that sorts the recovered events by date and hour (in chronological, ascending order)
+          _project_list_exhausted: boolean that indicates if all the known projects have been treated (events recovered)
+    """
+
+    def __init__(self):
+        self._recent_projects_ids: list[Project_ID] = []
+        self._known_events: dict[Event_ID, Project_ID] = {}
+        self._timestamp_events: Dict[Timestamp, HourlyEvents] = {}
+        self._project_list_exhausted: bool = False
+
+
+    def add_projects(self, projects: list[GitLabProject]):
+        for project in projects:
+            self._recent_projects_ids.append(object_id(project))
+
+
+    def pop_project_id(self):
+        return self._recent_projects_ids.pop(0)
+
+
+    def get_nb_projects(self):
+        return len(self._recent_projects_ids)
+
+
+    def is_project_list_exhausted(self):
+        return len(self._recent_projects_ids) == 0
+
+
+    def add_event_known(self, event_id: int, project_id: int):
+        self._known_events[event_id] = project_id
+
+
+    def is_event_known(self, event_id: int):
+        return event_id in self._known_events
+
+
+    def get_known_timestamps(self):
+        return list(self._timestamp_events.keys())
+
+
+    def store_event(self, event: GitLabEvent):
+        """
+        Store the event in the according timestamp (date and hour) in chronological order
+        """
+        event_timestamp = event_creation_date(event).strftime(JSON_DATE_FORMAT)
+
+        if event_timestamp not in self._timestamp_events:
+            new_entry: HourlyEvents = {'fetched_new_events': True, 'events': []}
+            self._timestamp_events.update({event_timestamp: new_entry})
+
+        target_slot = self._timestamp_events[event_timestamp]
+        insort(target_slot['events'], event, key=event_creation_date)
+
+        target_slot['fetched_new_events'] = True
+
+
+    def are_new_events_found(self):
+        known_timestamps = self.get_known_timestamps()
+
+        if len(known_timestamps) == 0:
+            return False
+
+        else:
+            for timestamp in known_timestamps:
+                if self.is_timestamp_updated(timestamp):
+                    return True
+
+        return False
+
+
+    def is_timestamp_updated(self, timestamp: Timestamp):
+        return self._timestamp_events[timestamp]['fetched_new_events']
+
+
+    def get_hourly_events(self, timestamp: Timestamp):
+        return self._timestamp_events[timestamp]['events']
+
+
+    def set_no_new_events(self, timestamp: Timestamp):
+        self._timestamp_events[timestamp]['fetched_new_events'] = False
+
+
+class TriggerCrawlTracker:
+    """
+    Track the number of projects and events recovered between two crawls
+
+    Attributes:
+        _nb_recovered_projects: number of recovered projects
+        _projects_recovery_time: total duration of project recovery (parallelized recovery)
+        _projects_response_time: list of all the projects request response time
+        _nb_recovered_events: number of recovered events
+        _events_response_time: list of all the events request response time
+        _nb_request_errors: number of failed requests (e.g., timeout, connection error)
+    """
+
+    def __init__(self):
+        self._nb_recovered_projects: int = 0
+        self._projects_recovery_time: Optional[float] = None
+        self._projects_response_time: list[float] = []
+        self._nb_recovered_events: int = 0
+        self._events_response_time: list[float] = []
+        self._nb_request_errors: int = 0
+
+
+    def increase_nb_recovered_projects(self, nb_projects: int):
+        self._nb_recovered_projects += nb_projects
+
+
+    def get_nb_recovered_projects(self):
+        return self._nb_recovered_projects
+
+
+    def set_projects_response_time(self, response_time: list[float]):
+        self._projects_response_time = response_time
+
+
+    def get_projects_response_time(self):
+        return self._projects_response_time
+
+
+    def set_projects_recovery_time(self, recovery_time: float):
+        self._projects_recovery_time = recovery_time
+
+
+    def get_projects_recovery_time(self):
+        return self._projects_recovery_time
+
+
+    def increase_nb_recovered_events(self, nb_events: int):
+        self._nb_recovered_events += nb_events
+
+
+    def get_nb_recovered_events(self):
+        return self._nb_recovered_events
+
+
+    def extend_events_response_time(self, response_time: list[float]):
+        self._events_response_time.extend(response_time)
+
+
+    def get_events_response_time(self):
+        return self._events_response_time
+
+
+    def increment_nb_request_errors(self):
+        self._nb_request_errors += 1
+
+
+    def get_nb_request_errors(self):
+        return self._nb_request_errors
+
+
+    def reset_tracker(self):
+        self._nb_recovered_projects = 0
+        self._projects_response_time = []
+        self._projects_recovery_time = None
+        self._nb_recovered_events = 0
+        self._events_response_time = []
+        self._nb_request_errors = 0
